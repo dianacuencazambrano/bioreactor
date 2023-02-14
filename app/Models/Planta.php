36ed96795bb0a8947e15d4acffdbcdb344a80b17
Diana <diana.cuenca.zambrano@udla.edu.ec>
2023-02-13 23:11:43 -0500

    first commit

---
+++ app/Models/Planta.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Planta extends Model
+{
+    use HasFactory;
+
+    protected $table = 'plantas';
+    protected $primaryKey = 'id_configuracion';
+
+    protected $fillable = ['nombre_planta','tiempo_espera','tiempo_sumergido','comentarios'];
+    
+}
